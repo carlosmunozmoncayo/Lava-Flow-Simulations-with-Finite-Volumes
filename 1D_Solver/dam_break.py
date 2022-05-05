95816d904d0181c47cf2c8a31d831283f1953799
carlosmunozmoncayo <carlos.munozmoncayo@kaust.edu.sa>
2022-05-05 10:29:12 +0300

    Add bathymetry tests and plotting to pyclaw script

--- 1D_Solver/dam_break.py
+++ 1D_Solver/dam_break.py
@@ -24,7 +24,11 @@ depth=0
 momentum_U=1
 momentum_T=2
 num_eqn=3
-num_waves=3 
+num_waves=3
+grav=9.8
+
+lowerx=-5.0
+upperx=5.0
 
 
 def get_lava_parameters(lava_flow):
@@ -41,17 +45,43 @@ def get_lava_parameters(lava_flow):
     return rho,b,cp,kappa,lambd,Tc,T0,mu_r
 
 def qinit(state,IC='dam_break'):
+    q=state.q
     x0=0.
     xc = state.grid.x.centers
     rho,b,cp,kappa,lambd,Tc,T0,mu_r=get_lava_parameters("Etna")
+    T_env=300
+
     if IC=='dam-break':
-        hl = 3.
+        bath=np.zeros(np.size(xc))
+        state.aux[0,:]= bath
+        zl = 3.
         ul = 0.
-        hr = 1.
+        zr = 1.
         ur = 0.
-        state.q[depth,:] = hl * (xc <= x0) + hr * (xc > x0)
-        state.q[momentum_U,:] = hl*ul * (xc <= x0) + hr*ur * (xc > x0)
-        state.q[momentum_T,:] = hl*T0 * (xc <= x0) + hr*T0 * (xc > x0)
+        hl= (zl-bath)*(xc<=x0)
+        hr= (zr-bath)*(xc>x0)
+        q[depth,:] = hl+hr
+        q[momentum_U,:] = hl*ul + hr*ur
+        q[momentum_T,:] = hl*T0 + hr*T0
+    elif IC=='stationary-state':
+        bath=np.zeros(np.size(xc))
+        state.aux[0,:]= bath
+        #temp=T0*(xc>=-1)*(xc<=1)+T_env*(xc<-1)+T_env*(xc>1)
+        q[depth,:] = 1- state.aux[0, :]
+        q[momentum_U,:] = 0.
+        q[momentum_T,:] = T0*state.q[depth,:]
+    elif IC=='two-bumps':
+        state.aux[0, :] = 0.8 * np.exp(-xc**2 / 0.2**2) - 1.0
+        q[depth, :] = 0.1 * np.exp(-(xc + 0.4)**2 / 0.2**2) - state.aux[0, :]
+        q[momentum_U, :] = 0.0
+        q[momentum_T, :] = T0*q[depth, :]
+    elif IC=='slope':
+        bath=0.4*xc+1
+        state.aux[0,:]= bath
+        q[depth,:] = 5- state.aux[0, :]
+        q[momentum_U,:] = 0.
+        q[momentum_T,:] = T0*q[depth,:] 
+
 #    elif IC=='2-shock':
 #        hl = 1.
 #        ul = 1.
@@ -65,6 +95,7 @@ def qinit(state,IC='dam_break'):
 #        state.q[momentum,:] = 0.
 
 #Defining source term
+
 def source_step_Lava_Flow(solver,state,dt):
     """
     Source term integration, this handles bathymetry and the temperature
@@ -72,35 +103,38 @@ def source_step_Lava_Flow(solver,state,dt):
     This is a Clawpack-style source term routine, which approximates
     the integral of the source terms over a step.
     """
-    #Obtaining sparameters ccharacteristic to the lava
+    #Obtaining parameters characteristic to the lava
     rho,b,cp,kappa,lambd,Tc,T0,mu_r=get_lava_parameters("Etna")
     nu_r=mu_r/rho
     T_env=300
 
-    #Doing several time integrations of source term to avoid stiffness
-    ddt=dt/10
-    if True:
-        q = state.q
+    #Obtaining data from state
+    q = state.q
+    bath=state.aux
+    xc = state.grid.x.centers
 
-        h = q[0,:]
-        u   = q[1,:]/h
-        T   = q[2,:]/h
-        #T  = q[3,:,:]/h
+    #Computing slope for bathymetry
+    dx=xc[3]-xc[2]
+    dHdx=np.zeros(np.size(bath[0]))
+    dHdx[1:]=(1/dx)*(bath[0,1:]-bath[0,0:-1])
+    dHdx[0]=dHdx[1]
+ 
 
-        #Computing more parameters (these are for Etna lava flows)
+    h = q[0,:]
+    u   = q[1,:]/h
+    T   = q[2,:]/h
 
-        H=(3*1.e-6)/h
-        E=1.5*1.e-15
-        W=2*1.e-6
-        K=(4*1.e-3)/h
+    #Computing more parameters (these are for Etna lava flows)
+    H=(3*1.e-6)/h
+    E=1.5*1.e-15
+    W=2*1.e-6
+    K=(4*1.e-3)/h
+    
+    #Integrating one step in time
+    q[1,:] = (h**2)*q[1,:]/(h**2+3*nu_r*dt*np.exp(-b*(T-T0)))-grav*dt*h*dHdx
+    q[2,:] = q[2,:]+dt*(-E*(T**4-T_env**4)-W*(T-T_env)-H*(T-Tc)+K*(u**2)*np.exp(-b*(T-T0)))
+    #print(q[2,50])
 
-        #q[0,:,:] = q[0,:,:] - dt/rad * qstar[2,:,:]
-        q[1,:] = (h**2)*q[1,:]/(h**2+3*nu_r*dt*np.exp(-b*(T-T0)))
-        #q[2,:] = (h**2)*q[2,:]/(h**2+3*nu_r*ddt*np.exp(-b*(T-T0)))
-        q[2,:] = q[2,:]+dt*(-E*(T**4-T_env**4)-W*(T-T_env)-H*(T-Tc)+K*(u**2)*np.exp(-b*(T-T0)))
-    else:
-        q = state.q
-        q[2,:]=4000
 
 def setup(use_petsc=False,kernel_language='Fortran',outdir='./_output',solver_type='classic',
           riemann_solver='monthe', disable_output=False):
@@ -121,9 +155,10 @@ def setup(use_petsc=False,kernel_language='Fortran',outdir='./_output',solver_ty
  
     if solver_type == 'classic':
         solver = pyclaw.ClawSolver1D(rs)
-        solver.order=1
-        #solver.limiters = pyclaw.limiters.tvd.vanleer
-        #solver.step_source=source_step_Lava_Flow
+        #solver.order=1
+        solver.limiters = pyclaw.limiters.tvd.vanleer
+        #Adding source term to conservation laws
+        solver.step_source=source_step_Lava_Flow
     elif solver_type == 'sharpclaw':
         solver = pyclaw.SharpClawSolver1D(rs)
 
@@ -131,24 +166,27 @@ def setup(use_petsc=False,kernel_language='Fortran',outdir='./_output',solver_ty
 
     solver.bc_lower[0] = pyclaw.BC.extrap
     solver.bc_upper[0] = pyclaw.BC.extrap
+    #Auxiliary vector will contain bathymetry
+    solver.aux_bc_lower[0] = pyclaw.BC.extrap
+    solver.aux_bc_upper[0] = pyclaw.BC.extrap
 
     solver.num_eqn=num_eqn
     solver.num_waves=num_waves
-
-    xlower = -5.0
-    xupper = 5.0
+   
+    xlower = lowerx
+    xupper = upperx
     mx = 500
     x = pyclaw.Dimension(xlower,xupper,mx,name='x')
     domain = pyclaw.Domain(x)
-    state = pyclaw.State(domain,num_eqn)
+    state = pyclaw.State(domain,num_eqn,num_aux=1)
 
     # Gravitational constant
-    state.problem_data['grav'] = 1.0
+    state.problem_data['grav'] = grav
     state.problem_data['dry_tolerance'] = 1e-3
     state.problem_data['sea_level'] = 0.0
     
 
-    IC='dam-break'
+    IC='slope'
     qinit(state,IC=IC)
 
 
@@ -156,11 +194,12 @@ def setup(use_petsc=False,kernel_language='Fortran',outdir='./_output',solver_ty
     claw.keep_copy = True
     if disable_output:
         claw.output_format = None
-    claw.tfinal = 2.0
+    claw.tfinal = 5.0
     claw.solution = pyclaw.Solution(state,domain)
     claw.solver = solver
     claw.outdir = outdir
     claw.setplot = setplot
+    claw.write_aux_init = True
 
     return claw
 
@@ -175,21 +214,44 @@ def setplot(plotdata):
     """ 
     plotdata.clearfigures()  # clear any old figures,axes,items data
 
+
+
+    # Plot variables
+    def bathy(current_data):
+        return current_data.aux[0, :]
+
+    def eta(current_data):
+        return current_data.q[0, :] + bathy(current_data)
+
+    def velocity(current_data):
+        return current_data.q[1, :] / current_data.q[0, :]
+
+    def temperature(current_data):
+        return current_data.q[2, :] / current_data.q[0, :]
+
+    rgb_converter = lambda triple: [float(rgb) / 255.0 for rgb in triple]
+
     # Figure for depth
     plotfigure = plotdata.new_plotfigure(name='Water height', figno=0)
-
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.xlimits = [-5.0,5.0]
+    plotaxes.xlimits = [lowerx,upperx]
     plotaxes.title = 'Water height'
     plotaxes.axescmd = 'subplot(311)'
 
     # Set up for item on these axes:
-    plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = depth
-    plotitem.plotstyle = '-'
-    plotitem.color = 'b'
-    plotitem.kwargs = {'linewidth':3}
+    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
+    plotitem.plot_var = eta
+    plotitem.plot_var2= bathy
+    plotitem.color = rgb_converter((67,183,219))
+    
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.plot_var = bathy
+    plotitem.color = 'k'
+
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.plot_var = eta
+    plotitem.color = 'k'
 
     # Figure for momentum[1]
     #plotfigure = plotdata.new_plotfigure(name='Momentum', figno=1)
@@ -197,15 +259,15 @@ def setplot(plotdata):
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
     plotaxes.axescmd = 'subplot(312)'
-    plotaxes.xlimits = [-5.0,5.0]
+    plotaxes.xlimits = [lowerx,upperx]
     plotaxes.title = 'Momentum'
 
     # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = momentum_U
+    plotitem.plot_var = velocity#momentum_U
     plotitem.plotstyle = '-'
     plotitem.color = 'b'
-    plotitem.kwargs = {'linewidth':3}
+    plotitem.kwargs = {'linewidth':1}
     
     #Figure for momentum[2]
     #plotfigure = plotdata.new_plotfigure(name='Momentum', figno=1)
@@ -213,15 +275,15 @@ def setplot(plotdata):
     # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
     plotaxes.axescmd = 'subplot(313)'
-    plotaxes.xlimits = [-5.0,5.0]
+    plotaxes.xlimits = [lowerx,upperx]
     plotaxes.title = 'Momentum_T'
 
     # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = momentum_T
+    plotitem.plot_var = temperature#momentum_T
     plotitem.plotstyle = '-'
     plotitem.color = 'b'
-    plotitem.kwargs = {'linewidth':3}
+    plotitem.kwargs = {'linewidth':1}
     return plotdata
 
 
